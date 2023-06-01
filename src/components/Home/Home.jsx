1775970093220fde711d7e1d7a3c42770eba60f6
Md H H Martin <engr.bmhhmartin@gmail.com>
2023-06-01 12:12:09 +0600

    React js currency converter

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
+
+const Home = () => {
+
+    return (
+            <Container>
+                <Row>
+                    <h2>Home</h2>
+                </Row>
+            </Container>
+    );
+}
+
+export default Home;
